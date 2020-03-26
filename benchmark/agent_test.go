7194ce5edce06628ea768bc7cb951029293c643b
Max Mathys <threadox@gmail.com>
2020-03-26 20:12:56 +0100

    doh.

--- benchmark/agent_test.go
+++ benchmark/agent_test.go
@@ -6,6 +6,8 @@ import (
 	"github.com/mmathys/acfts/common"
 	"github.com/mmathys/acfts/util"
 	"github.com/mmathys/acfts/wallet"
+	"os"
+	"strconv"
 	"sync"
 	"testing"
 	"time"
@@ -35,36 +37,57 @@ func simpleAgent(a common.Agent, wg *sync.WaitGroup) {
 }
 
 // there are 16 clients
-func testAgents(t *testing.T) {
-	common.InitAddresses("../topologies/localSimple.json")
-
+func testAgentsMultipleParallel(t *testing.T) {
 	clients := common.GetClients()
 	maxClients := len(clients)
-	delay := 500 * time.Millisecond
 
 	for numClients := 9; numClients <= maxClients; numClients++ {
-		msg := fmt.Sprintf("numClients: %d", numClients)
-		t.Run(msg, func(t *testing.T) {
-			numTx := 100000
-			var wg sync.WaitGroup
-			topology := clients[:numClients]
-
-			for _, addr := range topology {
-				a := common.Agent{NumTransactions: numTx, StartDelay: delay, Address: addr, Topology: topology}
-				wg.Add(1)
-				go simpleAgent(a, &wg)
-			}
-
-			wg.Wait()
-		})
+		testAgents(t, numClients)
 	}
 }
 
+func testAgents(t *testing.T, numClients int) {
+	delay := 500 * time.Millisecond
+	clients := common.GetClients()
+	msg := fmt.Sprintf("numClients: %d", numClients)
+	t.Run(msg, func(t *testing.T) {
+		numTx := 100000
+		var wg sync.WaitGroup
+		topology := clients[:numClients]
+
+		for _, addr := range topology {
+			a := common.Agent{NumTransactions: numTx, StartDelay: delay, Address: addr, Topology: topology}
+			wg.Add(1)
+			go simpleAgent(a, &wg)
+		}
+
+		wg.Wait()
+	})
+}
+
 func TestAgentsREST(t *testing.T) {
-	testAgents(t)
+	common.InitAddresses("../topologies/localSimple.json")
+	testAgentsMultipleParallel(t)
 }
 
 func TestAgentsRPC(t *testing.T) {
 	client.SetAdapterMode("rpc")
-	testAgents(t)
+	common.InitAddresses("../topologies/localSimple.json")
+	testAgentsMultipleParallel(t)
+}
+
+func TestAgentsRPCFixed(t *testing.T) {
+	args := os.Args
+
+	numClients, err := strconv.Atoi(args[len(args)-1])
+	if err != nil {
+		panic(err)
+	}
+
+	client.SetAdapterMode("rpc")
+
+	topo := args[len(args)-2]
+	common.InitAddresses(topo)
+
+	testAgents(t, numClients)
 }
