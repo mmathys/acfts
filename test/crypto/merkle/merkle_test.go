9579f3f2a14d1286951b4d77ffe3276926f4c8b8
Max Mathys <threadox@gmail.com>
2020-08-09 19:15:58 +0200

    completed merkle tests

--- test/crypto/merkle/merkle_test.go
+++ test/crypto/merkle/merkle_test.go
@@ -7,31 +7,31 @@ import (
 	"testing"
 )
 
-func sign(key *common.Key, numLeaves int) (hashes [][]byte, sigs []*common.Signature) {
-
+func hashes(numLeaves int) [][]byte {
 	// prepare hashes
-	hashes = make([][]byte, numLeaves, 64)
+	var hashes [][]byte
 	for i := 0; i < numLeaves; i++ {
-		rand.Read(hashes[i])
+		hash := make([]byte, 64)
+		rand.Read(hash)
+		hashes = append(hashes, hash)
 	}
-
-	// sign
-	sigs = key.SignMultipleMerkle(hashes)
-	return
+	return hashes
 }
 
 func TestSignMerkle(t *testing.T) {
 	const NumLeaves = 32
 	key := common.GenerateKey(common.ModeMerkle, 0)
 
-	_, sigs := sign(key, NumLeaves)
+	hashes := hashes(NumLeaves)
+	sigs := key.SignMultipleMerkle(hashes)
 	fmt.Println(len(sigs))
 }
 
 func TestVerifyMerkle(t *testing.T) {
 	const NumLeaves = 32
 	key := common.GenerateKey(common.ModeMerkle, 0)
-	hashes, sigs := sign(key, NumLeaves)
+	hashes := hashes(NumLeaves)
+	sigs := key.SignMultipleMerkle(hashes)
 
 	for i := range sigs {
 		valid, err := common.Verify(sigs[i], hashes[i])
@@ -41,5 +41,28 @@ func TestVerifyMerkle(t *testing.T) {
 		if !valid {
 			t.Fatal("signature not valid")
 		}
+	}
+}
+
+// benchmarks a 32 signatures
+func BenchmarkSignMerkle32(b *testing.B) {
+	const NumLeaves = 32
+	hashes := hashes(NumLeaves)
+	key := common.GenerateKey(common.ModeMerkle, 0)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = key.SignMultipleMerkle(hashes)
+	}
+}
+
+// only verify a single merkle signature.
+func BenchmarkVerifyMerkle(b *testing.B) {
+	const NumLeaves = 32
+	hashes := hashes(NumLeaves)
+	key := common.GenerateKey(common.ModeMerkle, 0)
+	sigs := key.SignMultipleMerkle(hashes)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		common.Verify(sigs[0], hashes[0])
 	}
 }
