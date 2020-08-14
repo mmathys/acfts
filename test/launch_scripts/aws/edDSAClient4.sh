7e92c24f763e2ae9290e9715423ab708a2d6e606
Max Mathys <threadox@gmail.com>
2020-08-14 18:46:21 +0200

    client eddsa config

---
+++ test/launch_scripts/aws/edDSAClient4.sh
@@ -0,0 +1,4 @@
+export ADDRESS=5e7da91d8083e79d742f39a1e218c92fdfb8e5684b60c3d2d82bce1fcc2c8084
+export TOPOLOGY=edDSAAWS4
+export NUM_WORKERS=128
+/usr/local/bin/docker-compose up --detach --build agents
